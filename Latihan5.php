cfefb1c90cee28f99e2a14f8b9aab71c7e78c613
ronakamilia27rpl <46396999+ronakamilia27rpl@users.noreply.github.com>
2019-02-20 14:18:48 +0700

    selesai dikerjakan

---
+++ Latihan5.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Variabel antar Halaman</title>
+    </head>
+    <body>
+        <form action="variabel_hasil.php" mrthod="post">  <!-- POST =UNTUK MENAMPILKAN -->
+            Nama  :<input type="text" name="nama"> <br>
+            Email :<input type="text" name="email"> <br>
+        </form>
+    </body>
+</html>
